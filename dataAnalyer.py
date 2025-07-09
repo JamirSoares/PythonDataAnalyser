cd5159b4f7a3b0d5af831a2eb73d33ace460a166
Jamir Soares Rodrigues <jamir.rodrigues@netservice.com.br>
2025-07-09 15:32:14 -0300

    creation project

---
+++ dataAnalyer.py
@@ -0,0 +1,99 @@
+import requests
+import sys
+import time
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+eventos = []
+API_ROUTE = os.getenv('ApiRoute')
+BEARER = os.getenv('berearToken')
+def get_excel():
+
+    contador = 0
+
+    dataisrt = '2025-07-01'
+    datafsrt = '2025-07-09'
+
+    # Converte strings para datetime (sem chamar strftime)
+    datai = datetime.strptime(dataisrt, '%Y-%m-%d')
+    dataf = datetime.strptime(datafsrt, '%Y-%m-%d')
+
+    while datai <= dataf:
+        # Atualiza a quantidade de pontinhos de 0 a 3
+        pontos = '.' * (contador % 4)
+        mensagem = f"\rIniciando a coleta de dados{pontos}"
+        sys.stdout.write(mensagem)
+        sys.stdout.flush()
+
+        time.sleep(0.5)
+        data_formatada = datai.strftime('%Y-%m-%d')
+        url = API_ROUTE
+
+        headers = {
+            "authorization": BEARER,
+            "content-type": "application/json",
+        }
+
+        
+
+        payload = {
+            "id": 1006984,
+            "current_date": f"{data_formatada}T11:25:57.327Z"
+        }
+
+        response = requests.post(url, headers=headers, json=payload)
+
+        try:
+            response.raise_for_status()  # Verifica se a requisição foi bem-sucedida
+        except ValueError:
+            print("Resposta de texto:", response.text)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            for hora, lista_eventos in data.items():
+                for evento in lista_eventos:
+                    if 'ending' in evento:
+
+                        data_str = evento.get('beginning')
+                        data_str = evento.get('beginning')
+                        eventos.append({
+                            'hora': hora,
+                            'beginning': datetime.fromisoformat(evento.get('beginning')).strftime('%Y-%m-%d'),
+                            'ending': datetime.fromisoformat(evento.get('ending')).strftime('%Y-%m-%d'),
+                            'description': evento.get('description'),
+                            'url_id': evento.get('url_id'),
+                            'horas_Apontadas': (datetime.fromisoformat(evento.get('ending')) - datetime.fromisoformat(evento.get('beginning'))).total_seconds() / 60
+
+                        })
+
+        else:
+            print("Erro ao obter dados:", response.status_code)
+        datai += timedelta(days=1)  # Avança a data
+        contador += 1
+
+    print("\nColeta finalizada!")
+
+    df = pd.DataFrame(eventos)
+    df.to_csv("eventos.csv", index=False)
+    return eventos
+
+
+def get_graph():
+    
+    df = pd.read_csv("eventos.csv", usecols=['beginning', 'horas_Apontadas'])
+    df['beginning'] = pd.to_datetime(df['beginning'])
+
+    df = df.sort_values(by='beginning')
+    df.plot(x='beginning', y='horas_Apontadas', kind='line', figsize=(10, 5))
+    plt.xlabel('Data')
+    plt.ylabel('horas_Apontadas')
+    plt.show()
+
+
+get_excel()
+get_graph()
